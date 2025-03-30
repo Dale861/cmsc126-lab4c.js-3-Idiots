df2a3c5cf17df4ecb6cd3e9b0dd6a944fbc33eb8
Coi <ctoyco1@up.edu.ph>
2025-03-30 20:55:46 +0800

    Initial files with time-date button

---
+++ Lab4C.js
@@ -0,0 +1,17 @@
+function showCurrentDate() {
+    const currentDate = new Date();
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const year = currentDate.getFullYear();
+    const month = months[currentDate.getMonth()]; 
+    const day = currentDate.getDate();
+    const weekday = weekdays[currentDate.getDay()];
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    
+    const msg = `Today is ${month} ${day}, ${year}, ${weekday}. The current time is ${hours>= 12 ? hours - 12 : hours}:${minutes} ${hours >= 12 ? "PM" : "AM"}.`;
+
+    document.getElementById("current-date").innerText = msg;
+}
