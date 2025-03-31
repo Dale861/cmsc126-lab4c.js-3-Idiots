f30fc52d1171380e0bf823f495e5267cdb6f8111
Coi <ctoyco1@up.edu.ph>
2025-03-31 10:13:54 +0800

    Additional Input Verification

--- Lab4C.js
+++ Lab4C.js
@@ -9,13 +9,15 @@ function showCurrentDate() {
     const day = currentDate.getDate();
     const weekday = weekdays[currentDate.getDay()];
     const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
+    const minutes = currentDate.getMinutes().toString().padStart(2, "0");
     let hr;
 
     if (hours > 12) {
         hr = hours - 12;
     } else if (hours === 0) {
         hr = 12;
+    }else{
+        hr = hours;
     }
     
     const msg = `Today is ${month} ${day}, ${year}, ${weekday}. <br>The current time is ${hr}:${minutes} ${hours >= 12 ? "PM" : "AM"}.`;
@@ -41,6 +43,28 @@ function addStudent(event){
         return;
     }
 
+    const namePattern = /^[a-zA-Z\s]+ [a-zA-Z\s]+$/;
+    if (!namePattern.test(name)) {
+        alert("Ivalid Format: please input your full name (First Last).");
+        return;
+    }
+    // sa example mo sa ppt sir mayara to document.getElementById('usernameError').textContent = 'error chuchu'
+    //wala ko to sir kay ga gamit lng ko return. paano to ga work imo sir if wala return?
+
+    if (age < 18) {
+        alert("You too young, enroll in highschool first");
+        return;
+    }else if(age > 99){
+        alert("You're too old, rest now");
+        return;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9]+@up\.edu\.ph$/;
+    if(!emailPattern.test(email)){
+        alert("Invalid email format: please input valid UPmail (Name123@up.edu.ph)")
+        return;
+    }
+
     //kulang pa pang validate name, age, and email
 
     const student = {
@@ -59,7 +83,7 @@ function addStudent(event){
     // Display student info nga naka list
     const studentsNiNiko = document.getElementById("studentsNiNiko");
     const listItem = document.createElement("li");
-    listItem.innerHTML = `<strong>${name}</strong>, ${age} years old, studying <em>${course}</em> (${email})`;
+    listItem.innerHTML = `<strong>${name}</strong>{${studentNumber}}, ${age} years old, studying <em>${course}</em> (${email})`;
     studentsNiNiko.appendChild(listItem)
   
     // Clear form fields
